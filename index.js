0b0de3da190cbf4f4992ddf18c31475e18369e83
LirazGanon <105818149+LirazGanon@users.noreply.github.com>
2022-06-01 23:35:20 +0300

    add botton and mobile

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //check for Navigation Timing API support
 if (window.performance) {
   console.info("window.performance works fine on this browser");
+  document.querySelector(".reload-img").setAttribute("onclick", "onClickNuni()");
 }
 console.info(performance.navigation.type);
 if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
   console.info("This page is reloaded");
-
+  document.querySelector(".reload-img").setAttribute("onclick", "location.reload()");
 // Change the dice image
   var randomNumber1 = Math.ceil(Math.random() * 6); //1-6
   document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");//change Player1's dice image src
@@ -14,7 +15,7 @@ if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
 
 // Change 'h1' and 'p'w' depend on the winning player
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
     document.querySelectorAll("p")[0].innerHTML = "<strong>player1</strong>"
   } else if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
@@ -26,4 +27,9 @@ if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
 
 } else {
   console.info("This page is not reloaded");
+}
+
+function onClickNuni(){
+  alert("תהנה מהכפתור נוני!");
+  location.reload();
 }
